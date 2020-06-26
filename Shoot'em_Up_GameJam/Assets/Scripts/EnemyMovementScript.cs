a7d9ba770e08a10ac03316bf86b81ae2c32f0f42
Shawrex <38359132+Shawrex@users.noreply.github.com>
2020-06-26 17:28:43 +0200

    mouvement de l'ennemi 1

---
+++ Shoot'em_Up_GameJam/Assets/Scripts/EnemyMovementScript.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementScript : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private Rigidbody2D rb;
+    private GameObject ship;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        ship = GameObject.Find("Ship");
+    }
+
+    void Update()
+    {
+        RotateToPlayer();
+    }
+
+    private void RotateToPlayer() => transform.right = ship.transform.position - transform.position; //Rotate vers le joueur
+
+    private void FixedUpdate() //Se déplacer vers le joueur
+    {
+        Vector2 dir = (ship.transform.position - transform.position).normalized;
+        rb.position += dir * speed * Time.fixedDeltaTime;
+    }
+}
