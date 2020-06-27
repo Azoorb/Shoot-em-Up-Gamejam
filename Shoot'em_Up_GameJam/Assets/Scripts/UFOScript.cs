049188df8b7e9dd911bdfbc900082297914ced69
Shawrex <38359132+Shawrex@users.noreply.github.com>
2020-06-27 13:47:01 +0200

    UFO Ennemi

---
+++ Shoot'em_Up_GameJam/Assets/Scripts/UFOScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class UFOScript : MonoBehaviour
+{
+    void Start()
+    {
+        //Animation Setup
+        Animator[] animators = GetComponentsInChildren<Animator>();
+
+        Debug.Log(animators.Length);
+
+        foreach (Animator a in animators)
+        {
+            if (a.gameObject.name == "Left")
+                a.SetBool("Left", true);
+            else
+                Debug.Log(a.gameObject.name);
+        }
+    }
+}
