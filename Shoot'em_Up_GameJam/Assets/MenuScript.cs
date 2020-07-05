226b88ef0a105142c6731d44bd2749532bfa4134
Azoorb <leo.dornaz@gmail.com>
2020-07-05 16:54:25 +0200

    build

--- Shoot'em_Up_GameJam/Assets/MenuScript.cs
+++ Shoot'em_Up_GameJam/Assets/MenuScript.cs
@@ -7,7 +7,7 @@ public class MenuScript : MonoBehaviour
 {
     public void LoadGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
     }
 
     public void Exit()
